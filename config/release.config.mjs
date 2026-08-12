0146fb30b8cc7494a55c4ee0f8497a56fe8bc4fb
interc0der <86900618+interc0der@users.noreply.github.com>
2026-08-12 11:07:08 -0700

    fix: use pnpm lifecycle release checks

--- config/release.config.mjs
+++ config/release.config.mjs
@@ -5,7 +5,7 @@ export default {
   branchChannels: { develop: 'beta', main: 'latest' },
   prereleaseIds: { beta: 'b' },
   packageManager: 'pnpm',
-  checks: ['rune build', 'rune test'],
+  checks: ['pnpm build', 'pnpm test'],
   access: 'public',
   github: true,
 };
